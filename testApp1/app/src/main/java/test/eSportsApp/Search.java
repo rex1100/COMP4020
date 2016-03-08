01ed6fb07a940587113593f95d62c36b2cb31658
ThomasDty <dutianyithomas@gmail.com>
2016-03-08 00:53:18 -0600

    add the list to search

--- testApp1/app/src/main/java/test/eSportsApp/Search.java
+++ testApp1/app/src/main/java/test/eSportsApp/Search.java
@@ -1,23 +1,60 @@
 package test.eSportsApp;
 
+import android.app.usage.NetworkStats;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
+import java.util.*;
+import android.view.*;
+import android.app.*;
+import android.widget.*;
 import test.testapp1.R;
 
 /**
  * Created by Thomas on 2016/3/6.
  */
-public class Search extends AppCompatActivity {
+public class Search extends AppCompatActivity{
+
+    private List<String> data = new ArrayList<String>();
+    private Button searchButton;
+    private ListView listView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
+        data.add("Dota");
+        data.add("Dota2");
+        data.add("HOTs");
+        data.add("LOL");
+        data.add("Hs");
+        data.add("COD");
+        listView = new ListView(this);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        //searchButton.setOnClickListener(this);
+        //searchButton.setOnTouchListener(this);
+        //listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,getData()));
+        //setContentView(listView);
     }
 
+    public boolean onTouch(View v,MotionEvent event){
+        if(event.getAction()== MotionEvent.ACTION_UP){
+            listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,getData()));
+            setContentView(listView);
+        }
+        return false;
+    }
+
+    public void onClick(View v){
+        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,getData()));
+        setContentView(listView);
+    }
+
+    private List<String> getData(){
+        return data;
+    }
 
 
     public void backHome(View view)
